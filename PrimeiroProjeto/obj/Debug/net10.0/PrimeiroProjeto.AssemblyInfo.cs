77f55eb6df5d8df8ea3463955bafe38f65c492e8
guidev1911 <guilhermereal1911@gmail.com>
2026-01-23 19:12:45 -0300

    exercicio 16

--- PrimeiroProjeto/obj/Debug/net10.0/PrimeiroProjeto.AssemblyInfo.cs
+++ PrimeiroProjeto/obj/Debug/net10.0/PrimeiroProjeto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimeiroProjeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b851ef140d3a854413dbb4bda0b5893884b0ec2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f7bb2e7429f2a8509b7aef918c75702020fe5db")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimeiroProjeto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrimeiroProjeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
