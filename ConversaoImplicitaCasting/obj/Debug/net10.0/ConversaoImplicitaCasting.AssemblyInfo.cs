33b0b64b498e20cb673df97b4e33a87addf6bd45
guidev1911 <guilhermereal1911@gmail.com>
2026-01-27 21:48:44 -0300

    exercicio 19

--- ConversaoImplicitaCasting/obj/Debug/net10.0/ConversaoImplicitaCasting.AssemblyInfo.cs
+++ ConversaoImplicitaCasting/obj/Debug/net10.0/ConversaoImplicitaCasting.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConversaoImplicitaCasting")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77f55eb6df5d8df8ea3463955bafe38f65c492e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9688f766cfad79e9f67acf76a956170cc393aca7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConversaoImplicitaCasting")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConversaoImplicitaCasting")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- PrimeiroProjeto/obj/Debug/net10.0/PrimeiroProjeto.AssemblyInfo.cs
+++ PrimeiroProjeto/obj/Debug/net10.0/PrimeiroProjeto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimeiroProjeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91f8b9d8c88e2732371c1478fd123ca18df949b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9688f766cfad79e9f67acf76a956170cc393aca7")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimeiroProjeto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrimeiroProjeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
