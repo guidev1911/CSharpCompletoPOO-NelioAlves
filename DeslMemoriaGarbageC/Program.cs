c7cbf7baed9a01e7e2f1d21f6b924d5cdac6dff1
guidev1911 <guilhermereal1911@gmail.com>
2026-05-02 19:57:12 -0300

    desalocação de memória garbage collector e escopo local

---
+++ DeslMemoriaGarbageC/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Pessoa
+{
+    public string Nome;
+}
+
+class Program
+{
+    static void CriarPessoa()
+    {
+        Pessoa p = new Pessoa();
+        p.Nome = "João";
+
+        Console.WriteLine("Dentro do método");
+    }
+
+    static void Main()
+    {
+        CriarPessoa();
+
+        Console.WriteLine("Fim do programa");
+    }
+}
