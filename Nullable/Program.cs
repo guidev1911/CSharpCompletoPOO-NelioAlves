03c94cab7e4234868ab844ba9c6b81f565976dfa
guidev1911 <guilhermereal1911@gmail.com>
2026-05-02 21:52:32 -0300

    Nullable

---
+++ Nullable/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int? a = null;
+        int b = a ?? 10;
+
+        int? c = 5;
+        int d = c ?? 10;
+
+        Console.WriteLine(b);
+        Console.WriteLine(d);
+    }
+}
