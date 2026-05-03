d3e97503a35dd9f2fae1a17f26d66c0f33f599ff
guidev1911 <guilhermereal1911@gmail.com>
2026-05-02 22:10:05 -0300

    vetores 1 media entre valores

---
+++ Vetor1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double[] numeros = new double[3];
+
+        for (int i = 0; i < 3; i++)
+        {
+            Console.Write($"Digite o {i + 1}º número: ");
+            numeros[i] = double.Parse(Console.ReadLine());
+        }
+
+        double soma = 0;
+
+        for (int i = 0; i < 3; i++)
+        {
+            soma += numeros[i];
+        }
+
+        double media = soma / 3;
+
+        Console.WriteLine($"Média: {media}");
+    }
+}
