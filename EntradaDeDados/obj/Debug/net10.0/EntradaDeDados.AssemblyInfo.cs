c3eade72b8b2d922ca9c2eb126e3ba68fd49f580
guidev1911 <guilhermereal1911@gmail.com>
2026-05-02 22:19:04 -0300

    exercicio de fixaxão vetores 2

--- EntradaDeDados/obj/Debug/net10.0/EntradaDeDados.AssemblyInfo.cs
+++ EntradaDeDados/obj/Debug/net10.0/EntradaDeDados.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EntradaDeDados")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75a43011e2d71107c1bef4aae8d3e8d345ae6174")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3e97503a35dd9f2fae1a17f26d66c0f33f599ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("EntradaDeDados")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EntradaDeDados")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Vetor2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Produto
+{
+    public string Nome;
+    public double Preco;
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Quantos produtos? ");
+        int n = int.Parse(Console.ReadLine());
+
+        Produto[] produtos = new Produto[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine($"Produto {i + 1}:");
+
+            produtos[i] = new Produto();
+
+            Console.Write("Nome: ");
+            produtos[i].Nome = Console.ReadLine();
+
+            Console.Write("Preço: ");
+            produtos[i].Preco = double.Parse(Console.ReadLine());
+        }
+
+        double soma = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            soma += produtos[i].Preco;
+        }
+
+        double media = soma / n;
+
+        Console.WriteLine($"Média dos preços: {media}");
+    }
+}
