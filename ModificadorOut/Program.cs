e14000cc3bec115dc548d94a54ed15590d570b2d
guidev1911 <guilhermereal1911@gmail.com>
2026-05-07 23:24:20 -0300

    ModificadorOut

---
+++ ModificadorOut/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void GerarNumero(out int x)
+    {
+        x = 50;
+    }
+
+    static void Main()
+    {
+        int n;
+
+        GerarNumero(out n);
+
+        Console.WriteLine(n); // 50
+    }
+}
