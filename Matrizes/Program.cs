e81c8e3154b9e920c1f27221c718cdb95f71ccaa
guidev1911 <guilhermereal1911@gmail.com>
2026-05-10 19:03:26 -0300

    Matrizes

---
+++ Matrizes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[,] matriz = new int[2, 2];
+
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                Console.Write($"Digite [{i},{j}]: ");
+                matriz[i, j] = int.Parse(Console.ReadLine());
+            }
+        }
+
+        Console.WriteLine("\nMatriz:");
+
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                Console.Write(matriz[i, j] + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
