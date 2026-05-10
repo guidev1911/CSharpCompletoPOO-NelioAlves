919e6b553acb2c9d68091bae48ae45e16c9995aa
guidev1911 <guilhermereal1911@gmail.com>
2026-05-10 17:50:14 -0300

    Lista parte 1 Add, Insert, Remove, Count

---
+++ ListPart1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> nomes = new List<string>();
+
+        nomes.Add("Ana");
+        nomes.Add("Carlos");
+        nomes.Add("Maria");
+
+        nomes.Insert(1, "João");
+
+        Console.WriteLine("Quantidade: " + nomes.Count);
+
+        foreach (string nome in nomes)
+        {
+            Console.WriteLine(nome);
+        }
+
+        nomes.Remove("Carlos");
+
+        Console.WriteLine("\nDepois do Remove:");
+
+        foreach (string nome in nomes)
+        {
+            Console.WriteLine(nome);
+        }
+    }
+}
