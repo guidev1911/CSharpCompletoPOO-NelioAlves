3dc537208b48322af27140b037f65ab9915b5270
guidev1911 <guilhermereal1911@gmail.com>
2026-05-10 17:51:28 -0300

    Lista parte 2 Find, FindAll, RemoveAll

--- ListParte2/Program.cs
+++ ListParte2/Program.cs
@@ -1,1 +1,39 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> nomes = new List<string>()
+        {
+            "Ana",
+            "Carlos",
+            "Maria",
+            "Marcos",
+            "Amanda"
+        };
+
+        string primeiroM = nomes.Find(x => x[0] == 'M');
+
+        Console.WriteLine("Primeiro com M: " + primeiroM);
+
+        List<string> listaM = nomes.FindAll(x => x[0] == 'M');
+
+        Console.WriteLine("\nNomes com M:");
+
+        foreach (string nome in listaM)
+        {
+            Console.WriteLine(nome);
+        }
+
+        nomes.RemoveAll(x => x.Length > 5);
+
+        Console.WriteLine("\nDepois do RemoveAll:");
+
+        foreach (string nome in nomes)
+        {
+            Console.WriteLine(nome);
+        }
+    }
+}
