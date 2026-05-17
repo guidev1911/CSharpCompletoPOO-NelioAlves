2f612a499d8bd0030dbd2e3365aa859b212a3801
guidev1911 <guilhermereal1911@gmail.com>
2026-05-17 18:30:21 -0300

    UpDownCasting

---
+++ UpDownCasting/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Animal
+{
+    public void FazerSom()
+    {
+        Console.WriteLine("Som genérico");
+    }
+}
+
+class Cachorro : Animal
+{
+    public void Latir()
+    {
+        Console.WriteLine("Au au!");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // UPCASTING
+        Animal a = new Cachorro();
+
+        a.FazerSom();
+
+        // a.Latir(); // erro
+
+        // DOWNCASTING
+        Cachorro c = (Cachorro)a;
+
+        c.Latir();
+    }
+}
