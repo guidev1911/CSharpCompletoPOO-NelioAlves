6e776a7c9cb709d12eb86bd7db37a7994f031959
guidev1911 <guilhermereal1911@gmail.com>
2026-05-17 19:23:50 -0300

    MetodosSelados

---
+++ MetodosSelados/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+sealed class Animal
+{
+    public void FazerSom()
+    {
+        Console.WriteLine("Som");
+    }
+}
+
+// ERRO
+// class Cachorro : Animal
+// {
+// }
+
+class Pessoa
+{
+    public virtual void Apresentar()
+    {
+        Console.WriteLine("Olá");
+    }
+}
+
+class Cliente : Pessoa
+{
+    public sealed override void Apresentar()
+    {
+        Console.WriteLine("Cliente");
+    }
+}
+
+// ERRO
+// class ClienteVip : Cliente
+// {
+//     public override void Apresentar()
+//     {
+//     }
+// }
+
+class Program
+{
+    static void Main()
+    {
+        Cliente c = new Cliente();
+
+        c.Apresentar();
+    }
+}
