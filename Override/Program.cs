c255378cb29507662bb32fa23704c16829575c76
guidev1911 <guilhermereal1911@gmail.com>
2026-05-17 19:19:30 -0300

    Override

---
+++ Override/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Funcionario
+{
+    public virtual double CalcularBonus()
+    {
+        return 1000;
+    }
+}
+
+class Gerente : Funcionario
+{
+    public override double CalcularBonus()
+    {
+        return base.CalcularBonus() + 2000;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Funcionario f = new Funcionario();
+        Gerente g = new Gerente();
+
+        Console.WriteLine(f.CalcularBonus());
+        Console.WriteLine(g.CalcularBonus());
+    }
+}
