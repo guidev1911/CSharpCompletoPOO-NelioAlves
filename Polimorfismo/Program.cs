eecd6f4ffbe45a169e47a0abb06bc70e706d1bff
guidev1911 <guilhermereal1911@gmail.com>
2026-05-17 19:31:04 -0300

    Polimorfismo

---
+++ Polimorfismo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Funcionario
+{
+    public virtual double CalcularPagamento()
+    {
+        return 2000;
+    }
+}
+
+class Freelancer : Funcionario
+{
+    public override double CalcularPagamento()
+    {
+        return 3500;
+    }
+}
+
+class Estagiario : Funcionario
+{
+    public override double CalcularPagamento()
+    {
+        return 1200;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        List<Funcionario> funcionarios = new List<Funcionario>();
+
+        funcionarios.Add(new Funcionario());
+        funcionarios.Add(new Freelancer());
+        funcionarios.Add(new Estagiario());
+
+        foreach (Funcionario f in funcionarios)
+        {
+            Console.WriteLine(f.CalcularPagamento());
+        }
+    }
+}
